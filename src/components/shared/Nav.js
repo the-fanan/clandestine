2cd6c0d032725589d1bcd71cc3061f5bcb1aa0f3
Fanan Dala <fanan.dala@yahoo.com>
2020-04-23 07:43:32 +0100

    bugfix: broken nav on login

--- src/components/shared/Nav.js
+++ src/components/shared/Nav.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, {Fragment} from "react";
 import { NavLink, withRouter } from "react-router-dom";
 import auth0Client from '../../Auth';
 
 const Nav = props => {
-	 const signOut = () => {
-	 	auth0Client.signOut();
-	 	props.history.replace('/');
-	 };
+	const signOut = () => {
+	auth0Client.signOut();
+	props.history.replace('/');
+	};
 
 	return (
 		<nav className="cl-nav">
-			<div className="container">
+			<div className="container-fluid">
 				<div className="row justify-content-between">
-					<div className="col-9 col-md-3">
+					<div className="col-9 col-md-2">
 						<div className="banner d-flex justify-content-between">
 							<NavLink to="/" className="navbar-brand">Clandestine</NavLink>
 							<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,21 +21,26 @@ const Nav = props => {
 						</div>
 					</div>
 
-					<div className="col-12 col-md-9 d-flex justify-content-end">
+					<div className="col-12 col-md-10 d-flex justify-content-end">
 						<ul className="nav">
-							<li className="nav-item">
 								{
 									!auth0Client.isAuthenticated() &&
-									<NavLink to="/" className="nav-link" onClick={auth0Client.signIn}>Log In/Sign Up</NavLink>
+									<li className="nav-item">
+										<NavLink to="/" className="nav-link" onClick={auth0Client.signIn}>Log In/Sign Up</NavLink>
+									</li>
 								}
 								{
 									auth0Client.isAuthenticated() &&
-									<div>
-										<label className="nav-btn">{auth0Client.getProfile().name}</label>
-										<NavLink to="/" className="nav-link" onClick={() => {signOut()}}>Sign Out</NavLink>
-									</div>
+									<Fragment>
+										<li className="nav-item">
+											<NavLink to="/profile" className="nav-link">{auth0Client.getProfile().name}</NavLink>
+										</li>
+										<li className="nav-item">
+											<NavLink to="/" className="nav-link" onClick={() => {signOut()}}>Sign Out</NavLink>
+										</li>
+									</Fragment>		
 								}
-							</li>
+							
 							<li className="nav-item">
 								<NavLink to="/global-leader-board" className="nav-link">Global Leader Board</NavLink>
 							</li>
